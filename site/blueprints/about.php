8f6e8384f1e7c0413651c895741c82890940e847
Bill Phan <billphan25@gmail.com>
2015-05-14 23:33:41 -0700

    Blueprint for About

---
+++ site/blueprints/about.php
@@ -0,0 +1,42 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: About
+pages:
+  hide: true
+files: false
+
+fields:
+  title:
+    label: History
+    type: textarea
+  mission:
+    label: Mission
+    type: textarea
+  tagline:
+    label: Tagline
+    type: text
+  photo:
+    label: Photo
+    type: image
+  members:
+    label: Members
+    type: textarea
+  name:
+    label: Name
+    type: text
+  bio:
+    label: Biography
+    type: textarea
+  photo:
+    label: Photo
+    type: image
+  title:
+    label: Title
+    type: text
+
+
+
+
+  
+
+

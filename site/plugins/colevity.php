657c9fdf317e545a85dcf53ab8a1cfdf95be9b9a
August W. Miller <hello@gusmiller.com>
2015-05-17 21:37:00 -0700

    Static methods for retrieving collections of content
    Most of these are used in the `home` controller

--- site/plugins/colevity.php
+++ site/plugins/colevity.php
@@ -6,10 +6,34 @@
     ];
 
     # Filter out number-only classes:
-    $classes = array_filter($classes, function($class) {
+    $classes = array_unique(array_filter($classes, function($class) {
       return !preg_match('/^\d+$/', $class);
-    });
+    }));
 
     return join($classes, ' ');
+  }
+
+  public static function performances_by_date () {
+    return page('performances')->children()->sortBy('date');
+  }
+
+  public static function workshops_by_date () {
+    return page('workshops')->children();
+  }
+
+  public static function classes_by_date () {
+    return page('classes')->children();
+  }
+
+  public static function all_programming_by_date () {
+    $programs = new Pages();
+    $programs->add(static::workshops_by_date());
+    $programs->add(static::classes_by_date());
+
+    return $programs->sortBy('date');
+  }
+
+  public static function troupe_members () {
+    return page('troupe')->children();
   }
 }

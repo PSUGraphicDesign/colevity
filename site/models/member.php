a55742baf89006c703226666c4109f5a4364ec96
August W. Miller <hello@gusmiller.com>
2015-05-17 21:38:32 -0700

    Member model to alias name and grab profile image
    Todo: Add fallback image

---
+++ site/models/member.php
@@ -0,0 +1,13 @@
+<? class MemberPage extends Page {
+  public function name () {
+    return $this->title();
+  }
+  
+  public function profile_image () {
+    if ( $this->hasImages() ) {
+      return $this->images()->first();
+    } else {
+      return false;
+    }
+  }
+}

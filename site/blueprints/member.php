70fe65fedc66de3a783d5e9c49ab8361787e967d
August W. Miller <hello@gusmiller.com>
2015-05-17 21:38:05 -0700

    Troupe members and member blueprints

---
+++ site/blueprints/member.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Troupe Member
+pages:
+  hide: true
+files: images
+
+fields:
+  title:
+    label: Member Name
+    type: text
+  bio:
+    label: Member Biographical Information
+    type: textarea
---
+++ site/blueprints/members.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Troupe Members
+pages: member
+files: false
+
+fields:
+  info:
+    label: Information
+    type: info
+    help: >
+      This is a place to add CoLevity performers. Please do not delete or rename this page! Add a new performer by clicking “Add,” to the left.
+  title:
+    label: Title
+    type: text

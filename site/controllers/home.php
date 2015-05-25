5b4a1683efcc4d1b42c7ea2edbb619fb3927bc38
Jessica Sadie Burchett <burchett@pdx.edu>
2015-05-25 14:56:39 -0700

    Display classes

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -6,7 +6,7 @@
     'giving' => page('giving'),
     'performances' => CL::performances_by_date(),
     'next_performance' => CL::performances_by_date()->first(),
-    'classes' => CL::classes_by_date(),
+    'classes' => page('classes'),
     'workshops' => CL::workshops_by_date(),
     'programs' => CL::all_programming_by_date(),
     'troupe' => CL::troupe_members()

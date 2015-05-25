58d3c9a7f704adc4860f94ef0b1b41ec40a6801f
Thomoore <Thomoore@pdx.edu>
2015-05-25 00:40:29 -0700

    Changed the styling on the location map

--- assets/js/colevity.js
+++ assets/js/colevity.js
@@ -13,7 +13,58 @@ function map_init (map) {
     zoom: zoom,
     center: new google.maps.LatLng(lat, lng),
     disableDefaultUI: true,
-    styles: [] // Replace with your own stylers!
+    styles: [
+  {
+    "featureType": "landscape.man_made",
+    "stylers": [
+      { "visibility": "simplified" },
+      { "saturation": -100 },
+      { "lightness": -20 }
+    ]
+  },{
+    "featureType": "landscape.natural",
+    "stylers": [
+      { "visibility": "simplified" },
+      { "saturation": -100 },
+      { "lightness": -30 }
+    ]
+  },{
+    "featureType": "poi",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "road",
+    "stylers": [
+      { "visibility": "simplified" },
+      { "saturation": -100 }
+    ]
+  },{
+    "featureType": "transit",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "administrative",
+    "stylers": [
+      { "visibility": "simplified" },
+      { "saturation": -100 },
+      { "lightness": -77 }
+    ]
+  },{
+    "stylers": [
+      { "saturation": -100 }
+    ]
+  },{
+    "featureType": "road",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      { "saturation": -100 },
+      { "lightness": 100 },
+      { "visibility": "simplified" }
+    ]
+  }
+]
   };
 
   map = new google.maps.Map(document.getElementById('map-canvas'),

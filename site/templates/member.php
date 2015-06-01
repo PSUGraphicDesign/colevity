abade1fbaa27bd89681ee51127675197d6ad9ad3
August Miller <hello@gusmiller.com>
2015-06-01 11:10:29 -0700

    A bit of responsive styling
    Columns can be two-thirds width on tablet

--- site/templates/member.php
+++ site/templates/member.php
@@ -7,11 +7,11 @@
 </article>
 
 <article id="<?= $page->uid() ?>" class="troupe-member-details">
-  <div class="column half">
+  <div class="column half tablet-two-thirds mobile-full">
     <h3><?= $page->title() ?></h3>
     <?= $page->bio()->kirbytext() ?>
   </div>
-  <div class="column half">
+  <div class="column half tablet-third mobile-full">
     <?= html::img($page->profile_image()->url(), ['alt' => $page->title()]) ?>
   </div>
 </article>

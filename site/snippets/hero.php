ab847e581a61b056cca366c75e4638126139f34d
Jessica Sadie Burchett <burchett@pdx.edu>
2015-06-01 21:42:13 -0700

    Hero Circle
    Hellz yeah

--- site/snippets/hero.php
+++ site/snippets/hero.php
@@ -1,21 +1,25 @@
 <section class="hero" style="background-image: url('<?= $page->hero_image() ?>');">
   <article class="centered-layout">
-    <div class="column two-thirds circle">
-      <h1><?= $featured->title() ?></h1>
-      <div class="featured-details">
-        <? if (count($featured->all_shows()) > 1 ) { ?>
-          <?= date('M jS', strtotime($featured->first_show()['date'])) ?> — <?= date('M jS', strtotime($featured->last_show()['date'])) ?>
-        <? } else { ?>
-          <?= $featured->date('M jS', 'opening_date') ?>
-        <? } ?>
+    <div class="column two-thirds">
+      <div class="circle">
+        <div class="featured-info">
+        <h1><?= $featured->title() ?></h1>
+        <div class="featured-details">
+          <? if (count($featured->all_shows()) > 1 ) { ?>
+            <?= date('M jS', strtotime($featured->first_show()['date'])) ?> — <?= date('M jS', strtotime($featured->last_show()['date'])) ?>
+          <? } else { ?>
+            <?= $featured->date('M jS', 'opening_date') ?>
+          <? } ?>
+        </div>
+        <div class= "buy-tickets-button">
+          <?= $featured->purchase_tickets_link('white') ?>
+        </div>
+        <div class="learn">
+          <h2>Learn more</h2>
+        </div>
+        <div id="triangle-down"></div>
       </div>
-      <div class= "buy-tickets-button">
-      <?= $featured->purchase_tickets_link('white') ?>
-    </div>
-      <div class="learn">
-        <h2>Learn more</h2>
       </div>
-      <div id="triangle-down"></div>
     </div>
 
   </article>

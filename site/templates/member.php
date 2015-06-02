3ada675b637b6d597e5ce9c259f9aafde06db32c
August W. Miller <hello@gusmiller.com>
2015-06-01 20:35:20 -0700

    Performances stylesheet

--- site/templates/member.php
+++ site/templates/member.php
@@ -1,7 +1,7 @@
 <article id="<?= $page->uid() ?>" class="troupe-member-details">
   <? snippet('modal-controls') ?>
   <div class="column half tablet-two-thirds mobile-full">
-    <h3><?= $page->title() ?></h3>
+    <h2><?= $page->title() ?></h2>
     <?= $page->bio()->kirbytext() ?>
   </div>
   <div class="column half tablet-third mobile-full">
--- site/templates/performance.php
+++ site/templates/performance.php
@@ -1,7 +1,7 @@
 <article id="<?= $page->uid() ?>" class="performance-details">
   <? snippet('modal-controls') ?>
   <div class="column half mobile-full">
-    <h3><?= $page->title() ?></h3>
+    <h2><?= $page->title() ?></h2>
     <?= $page->description()->kirbytext() ?>
   </div>
 </article>

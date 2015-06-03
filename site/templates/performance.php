3953986fe5df98644b55778c0a3b67d6b5b88c48
August Miller <hello@gusmiller.com>
2015-06-03 09:07:27 -0700

    AJAX'd performances have images
    @TJ-Butler will provide a slideshow plugin

--- site/templates/performance.php
+++ site/templates/performance.php
@@ -4,4 +4,15 @@
     <h2><?= $page->title() ?></h2>
     <?= $page->description()->kirbytext() ?>
   </div>
+  <? if ( $images->count() ) { ?>
+    <div class="column full">
+      <div class="slideshow">
+        <? foreach ( $images as $image ) { ?>
+          <div class="slide">
+            <?= html::img(thumb($image, ['width' => 1500])->url()) ?>
+          </div>
+        <? } ?>
+      </div>
+    </div>
+  <? } ?>
 </article>

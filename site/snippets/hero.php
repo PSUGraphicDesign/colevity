e61c8c71b8fe1c59ade1ceb5f6f194e065962180
Jessica Sadie Burchett <burchett@pdx.edu>
2015-06-03 09:15:42 -0700

    Buttons!

--- site/snippets/hero.php
+++ site/snippets/hero.php
@@ -11,7 +11,7 @@
             <?= $featured->date('M jS', 'opening_date') ?>
           <? } ?>
         </div>
-        <div class= "buy-tickets-button">
+        <div class= "buy-tickets button beige">
           <?= $featured->purchase_tickets_link('white') ?>
         </div>
         <div class="learn">

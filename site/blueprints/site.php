7e4e3715ee0a7ecb0f9370cf7a8be02453f79e93
August Miller <hello@gusmiller.com>
2015-06-07 16:14:57 -0700

    Site Blueprint

---
+++ site/blueprints/site.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Site
+pages: true
+files: image
+fields:
+  title:
+    label: Site Name
+    type:  text
+  description:
+    label: Text
+    type:  textarea
+    buttons: false
+    help: >
+      A succinct description of CoLevity. This will likely appear in search engine results!

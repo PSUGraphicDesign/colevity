f79e1eb880228cb08d285114a15cd8b7efcb1217
Thomoore <Thomoore@pdx.edu>
2015-06-09 09:41:06 -0700

    Added footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,2 +1,19 @@
 </body>
+  <footer>
+
+    <div class="column full footer">
+      <div class="footer-content three-quarters">
+        <div class="footer-segment column third"><p>Logo design: Tessa Millholin</p></div>
+        <div class="footer-segment column third"><p>Site design and developement: August miller, Sadie Burchett, Tyley Butler, Bill Phan, and Thomas Moore</p></div>
+        <div class="footer-segment column third"><p>Copyright CoLevity 2015</p></div>
+      </div>
+      <a href="colevity.com/panel"><p>Login</p></a>
+    </div>
+
+  </footer>
+
+
+
+
+
 </html>
